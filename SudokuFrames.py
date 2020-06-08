cdb6faf06792a9585d144250244d96cbdd9b2968
Kavija Karunarathna <kavisuper@gmail.com>
2020-06-08 06:01:54 +0530

    The program is functional. Have to implement dynamic value changing in the next commit.

--- SudokuFrames.py
+++ SudokuFrames.py
@@ -10,25 +10,29 @@ class BoardLabel:
         self.widget = tkinter.Label(frame, borderwidth=4, relief='groove', width=3, height=1, font="ABeeZee 25",
                                     fg="white", background="#138D75")
         self.widget.grid(row=row, column=col)
+        self.labelText = tkinter.StringVar()
+        self.widget.config(textvariable=self.labelText)
 
-    def add_to_label(self, char, widgets):
-        self.widget.config(text=char)
+    def add_to_label(self, char, widgets, string_vars):
+        self.labelText.set(char)
         widgets.append(self.widget)
+        string_vars.append(self.labelText)
 
 
 def create_board(window):
     widgets = []
+    string_vars = []
     board = window
     for row in range(9):
         for col in range(9):
             label = BoardLabel(board, row, col)
-            label.add_to_label(picked_board[row][col], widgets)
+            char = picked_board[row][col]
+            label.add_to_label(char, widgets, string_vars)
     board.grid(row=0, column=0, rowspan=9, columnspan=9)
     n = 9
     widgets = [widgets[i * n:(i + 1) * n] for i in range((len(widgets) + n - 1) // n)]
-    # widgets.pop()
-    # print(widgets)
-    return widgets
+    string_vars = [string_vars[i * n:(i + 1) * n] for i in range((len(string_vars) + n - 1) // n)]
+    return widgets, string_vars
 
 
 
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import tkinter
 from SampleSudokus import picked_board
 from time import sleep
 
+
 temp_board = picked_board[::]
 master = tkinter.Tk()
 master.title("SudokuSolver")
 master.geometry("603x500+500+170")
 Frame = tkinter.Frame(master, background="#138D75")
-widgets = SudokuFrames.create_board(Frame)
+widgets, string_vars = SudokuFrames.create_board(Frame)
 solve_btn = tkinter.PhotoImage(file="btn.png")
 Button = tkinter.Button(Frame, image=solve_btn, command=lambda: solve(temp_board, numbers))
 Button.grid(row=9, column=0, columnspan=9, sticky='ew')
@@ -42,9 +43,10 @@ def solve(board, number):
 
     for cha in number:
         if valid(board, cha, row, col):
-            widgets[row][col].config(text=cha)
             widgets[row][col].config(background="#D1F2EB")
             widgets[row][col].config(fg="#1B4F72")
+            string_vars[row][col].set(cha)
+            sleep(0.0001)
             board[row][col] = cha
 
             if solve(board, number):
@@ -52,7 +54,8 @@ def solve(board, number):
 
             widgets[row][col].config(background="#D1F2EB")
             widgets[row][col].config(fg="#1B4F72")
-            widgets[row][col].config(text=" ")
+            string_vars[row][col].set(" ")
+            sleep(0.0001)
             board[row][col] = " "
 
     return False
